7cc5f90ae36e8bc1930ba80fafcd327d610b3120
t3yamoto <3yamoto.dev@gmail.com>
2022-09-19 15:01:04 +0900

    Randomly displayed from 3 cats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
-const IndexPage = () => {
-    return <img src="https://cdn2.thecatapi.com/images/bpc.jpg" />;
+import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
+import { useState } from "react";
+
+const catImages: string[] = [
+    "https://cdn2.thecatapi.com/images/bpc.jpg",
+    "https://cdn2.thecatapi.com/images/eac.jpg",
+    "https://cdn2.thecatapi.com/images/6qi.jpg",
+];
+
+const randomCatImage = (): string => {
+    const index = Math.floor(Math.random() * catImages.length)
+    return catImages[index]
 }
+
+
+const IndexPage = () => {
+    const [catImageUrl, setCatImageUrl] = useState("https://cdn2.thecatapi.com/images/bpc.jpg")
+    const handleClick = () => {
+        setCatImageUrl(randomCatImage())
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>きょうのにゃんこ 🐱</button>
+            <div style={{ marginTop: 8}}>
+                <img src={catImageUrl} />
+            </div>
+        </div>
+    );
+}
+
 
 export default IndexPage
